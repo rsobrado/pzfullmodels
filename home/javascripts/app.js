b3e7d9a0b42ec28d7df4a6d4324a59aeb433a6db
rogers <rsobrado@criticalmass.com>
2013-07-02 19:45:55 -0600

    home image loader

--- home/javascripts/app.js
+++ home/javascripts/app.js
@@ -1,17 +1,23 @@
-;(function ($, window, undefined) {
+
+var photosetsIds = new Array();
+(function ($, window, undefined) {
   'use strict';
   var gallery_st;
   var $doc = $(document),
   Modernizr = window.Modernizr;
 
-  var $flickrAPI_Key = 'c3cadcbf024df6f25639c5e6f94484a9';
-  var $flickr_ID = '29587567%40N06';
+  var flickrAPI_Key = 'c3cadcbf024df6f25639c5e6f94484a9';
+  var flickr_ID = '29587567%40N06';
 
   $(document).ready(function() {
-
-   // flickr_photoset( $flickr_ID ,'72157630196621864');
-
-    getGalleries($flickrAPI_Key,$flickr_ID);
+    var index = 0;
+    getGalleries(flickrAPI_Key,flickr_ID);
+    setTimeout(function(){
+      var myVar=setInterval(function(){
+        flickr_photoset(flickrAPI_Key, photosetsIds[index]);
+        index += 1;
+      },500);
+    },1000);
   });
 
   // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
@@ -34,18 +40,13 @@
       jsonpCallback: 'jsonFlickrApi',
       success: function(data) {
         gallery_st = true; //to avoid reload info
-        console.log(data);
-
 
         $.each(data.photosets.photoset, function(i,photoset){ 
-
-          var gallery ='<li> <div class="gallery"><h1>'+photoset.title._content+'</h1></div></li>';
+          photosetsIds.push(photoset.id);
+          var gallery ='<li> <div class="gallery"><h1>'+photoset.title._content+'</h1><div id="set-'+photoset.id+'"></div></div></li>';
           $('#collections').append(gallery);
-
-          
-      });
-
-      }     
+        });
+      }
     });
   }
 
@@ -57,7 +58,7 @@
     $.ajax({ 
 
       type:"application/javascript",
-      url: 'http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key='+API_KEY+'&photoset_id='+PS_ID+'&format=json',
+      url: 'http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key='+API_KEY+'&photoset_id='+PS_ID+'&format=json&per_page=3',
       dataType: 'jsonp',
       jsonpCallback: 'jsonFlickrApi',
       success: function(data) {
@@ -67,9 +68,9 @@
 
           var images ='<img src="http://farm5.static.flickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'_m.jpg" class="box myphotos" rel="'+PS_ID+'" data-glisse-big="http://farm5.static.flickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'_b.jpg" title="'+photo.title+'" />';
 
-          $('#sets').append(images);  
+          $('#set-'+PS_ID).append(images);  
 
-          $('#sets').masonry({
+          $('#set-'+PS_ID).masonry({
             columnWidth: 300,
             itemSelector: '.box', 
             isAnimated: true,
@@ -78,11 +79,11 @@
               queue: false  
             }  
         }).imagesLoaded(function() {
-            $('#sets').masonry('reload');
-            });
+            $('#set-'+PS_ID).masonry('reload');
+        });
       });
 
-      $(function () {$('.myphotos').glisse({speed: 500, changeSpeed: 550, effect:'fade', fullscreen: true});}); 
+      $(function () {$('.myphotos').glisse({speed: 500, changeSpeed: 550, effect:'fade', fullscreen: false});}); 
 
       }     
     });
